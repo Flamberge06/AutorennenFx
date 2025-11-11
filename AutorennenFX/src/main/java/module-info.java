ea91f2b705d35715800fa8a3428a378ee23f2ef4
Flamberge06 <165439313+Flamberge06@users.noreply.github.com>
2025-11-11 12:13:19 +0100

    Delete AutorennenFX directory

--- AutorennenFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.autorennenfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-    opens com.example.autorennenfx to javafx.fxml;
-    exports com.example.autorennenfx;
-}
